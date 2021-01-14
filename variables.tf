f356d91bee10784da4603d3a50c96037bc2bfcd1
Paulo Mariano <paulo.azevedo@stone.com.br>
2021-01-14 16:38:34 -0300

    adds variable.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+# Global variables
+variable "region" {
+  description = "value"
+  default     = "us-west-2"
+}
+
+variable "access_key" {
+  description = "value"
+}
+
+variable "secret_key" {
+  description = "value"
+}
+
+variable "tags" {
+  description = "value"
+  default = {}
+}
+
+# EC2 variables
+variable "instance_count" {
+  description = "value"
+  default = 1
+}
+
+variable "instance_type" {
+  description = "value"
+  default = "t3.micro"
+}
