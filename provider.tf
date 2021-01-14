35a56d3c55da93b84916144813991c8c6f6c34b2
Paulo Mariano <paulo.azevedo@stone.com.br>
2021-01-14 16:39:49 -0300

    adds files to configure provider

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
